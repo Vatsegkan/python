4401210a65eb0d8a80a953bd8c2809c5bce17e97
vasilis <90329145+Vatsegkan@users.noreply.github.com>
2022-11-16 20:48:37 +0000

    ok

---
+++ Skript Beispile/zeichenketten.formatieren.py
@@ -0,0 +1,22 @@
+name = "Vasilis"
+print("Hallo {}".format(name))
+
+
+print("___________________________")
+
+
+print("ID: {}, Name: {}".format(123, 'Stefan'))
+
+
+print("___________________________")
+
+
+print("Name: {name}".format(name = 'Corina'))
+
+print("___________________________")
+
+print("Name: {[name]}".format({'name':'Corinaki'}))
+
+print("___________________________")
+
+print("Name: {2}, Vorname {0}".format('Vatsegkan','vasilis','zour'))
