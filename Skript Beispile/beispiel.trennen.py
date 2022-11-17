45ac94dff83b624d0994f25e72dd4a477e7a4367
vasilis <90329145+Vatsegkan@users.noreply.github.com>
2022-11-17 13:41:44 +0000

    ok

---
+++ Skript Beispile/beispiel.trennen.py
@@ -0,0 +1,11 @@
+text1 = "November rain"
+print(text1.split())
+
+print("_____________________________")
+
+text2 = "sweet#child#o'#mine"
+print(text2.split('#'))
+
+print("_____________________________")
+
+print(text2.split("#",1))
