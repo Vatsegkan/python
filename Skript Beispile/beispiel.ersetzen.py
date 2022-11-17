de7ecca50c4fb18cb1b2f2d0b1980ec008a795cf
vasilis <90329145+Vatsegkan@users.noreply.github.com>
2022-11-17 13:57:54 +0000

    ok

---
+++ Skript Beispile/beispiel.ersetzen.py
@@ -0,0 +1,8 @@
+text1 = "HELLO there"
+print(text1)
+print("___________________________")
+print(text1.lower())
+print("___________________________")
+print(text1.upper())
+print("___________________________")
+print(text1.capitalize())
