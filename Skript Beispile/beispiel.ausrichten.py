4e4f16bd216617b0b89020fdc758f7b58cc9fb02
vasilis <90329145+Vatsegkan@users.noreply.github.com>
2022-11-17 14:23:24 +0000

    ok

---
+++ Skript Beispile/beispiel.ausrichten.py
@@ -0,0 +1,9 @@
+text1 = "Hey ya"
+text2 = "in a gabba da vida"
+text3 = "23.456"
+
+print(text1.zfill(10))
+print("___________________________")
+print(text2.zfill(10))
+print("___________________________")
+print(text3.zfill(10))
