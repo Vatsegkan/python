165ef6216b489363c8433d46934f64e4038fa643
vasilis <90329145+Vatsegkan@users.noreply.github.com>
2022-11-17 14:33:37 +0000

    ok

---
+++ Skript Beispile/beispiel.testen.py
@@ -0,0 +1,8 @@
+text1 = "yabba dabba doo"
+print(text1.startswith("yabba"))
+print("___________________________")
+print(text1.startswith("dabba", 6, 12))
+print("___________________________")
+print(text1.startswith("dabba",6 ,8))
+print("___________________________")
+print(text1.endswith("doo"))
