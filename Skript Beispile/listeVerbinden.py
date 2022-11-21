dd4e08cb8827f2563cd167add93ab7e19265995f
vasilis <90329145+Vatsegkan@users.noreply.github.com>
2022-11-21 13:50:08 +0000

    ok

---
+++ Skript Beispile/listeVerbinden.py
@@ -0,0 +1,11 @@
+my_liste = [1, 2, 3]
+my_liste.extend([4, 5, 6])
+print(my_liste)
+
+my_liste = [1, 2, 3]
+my_liste.append(0)
+print(my_liste)
+
+new_my_liste = [1, 2, 3]
+add_my_liste = new_my_liste + [4, 5, 6]
+print(add_my_liste)
